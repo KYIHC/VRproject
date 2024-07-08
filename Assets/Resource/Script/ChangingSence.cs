b80840f8be0bbf9ad5395cb4256f77df24f1aee1
KimYoungJean <koj887174@gmail.com>
2024-07-09 04:12:43 +0900

    씬전환(수정)

--- Assets/Resource/Script/ChangingSence.cs
+++ Assets/Resource/Script/ChangingSence.cs
@@ -9,14 +9,31 @@ public class ChangingSence : MonoBehaviour
     public void ChangeSence(string senceName)
     {
         PlayerPrefs.SetString("lastLoadedScene", senceName);
-        SceneManager.LoadScene("loadingScene");
+        SceneManager.LoadScene("LoadingScene");
 
     }
     public void QuitGame()
     {
         
         Application.Quit(); 
+        //플레이모드 종료
 
+    }
+
+    public void LoadLastScene()
+    {
+        string lastLoadedScene = PlayerPrefs.GetString("lastLoadedScene");
+        SceneManager.LoadScene(lastLoadedScene);
+    }
+
+    private void Update()
+    {
+        //현재씬이 로딩씬이면
+        if (SceneManager.GetActiveScene().name == "LoadingScene")
+        {
+
+            LoadLastScene();
+        }
     }
 
 }
